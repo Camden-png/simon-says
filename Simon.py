e98c289ca3ddd8ae35358e389c720a07f6ad445d
Camden <46905140+Camden-png@users.noreply.github.com>
2021-02-20 19:36:00 -0800

    Add files via upload

--- Simon.py
+++ Simon.py
@@ -7,28 +7,29 @@ logo = """
 instructions = """Commands:
 • !simon score all - check all scores
 • !simon score me - check your score
-• !simon score name - check other player scores"""
+• !simon score <player> - check another player's score"""
 
 import os
 import sys
 import time
-import json
 import random
 import asyncio
 import platform
-import unicodedata
 
 try:
     import discord
+    from discord.utils import get
     from discord.ext import commands
 except ImportError:
     print("Error: no Discord library found!")
     sys.exit(1)
 
-wait = 30
+wait = 60
 word = ""
-words = []
 taken = []
+votes = []
+trick = False
+special = None
 channel = None
 
 bot = discord.Client()
@@ -40,165 +41,217 @@ def clear():
 
 async def start(token): global bot; await bot.start(token)
 
-def printer_1(string): print(f"Error: no {string} found!")
+def error(string): print(f"Error: no {string} found!"); return 1
 
-async def printer_2(mode, string, display = False):
-    global image
-    tone = 0xea3c50; image = "https://i.imgur.com/NcjngFw.png"
-    embed = discord.Embed(description = string, color = tone)
+async def printer(mode, string, display = False):
+    tones = 0xea3c50
+    image = "https://i.imgur.com/NcjngFw.png"
+    embed = discord.Embed(description = string, color = tones)
     if display: embed.set_thumbnail(url = image)
     await mode.send(embed = embed)
 
-def open_file(name_1, name_2 = None):
-    try: file = open(name_1, "r")
-    except: printer_1(name_1)
-    line = file.read()
-    if name_2 and not line: printer_1(name_2)
-    file.close()
-    if name_2: return line
-    else: return "Start"
-
 def main():
-    global bot; global logo; global words; clear()
-    token = open_file("Token.txt", "token")
-    if not token: return 1
-    if not open_file("Scores.txt"): return 1
-    lines = open_file("Random.txt", "words")
-    if not lines: return 1
-    words = lines.split("\n")
-    print(logo)
+    global bot, wait, logo
+    clear()
+    try: wait = abs(float(sys.argv[1]))
+    except: pass
+    name = "Token.txt"
+    try: file = open(name, "r")
+    except: return error(name)
+    token = file.read()
+    file.close()
+    if not token: return error("token")
+    name = "Scores.txt"
+    try: file = open(name, "r")
+    except: return error(name)
+    file.close()
+    name = "Random.txt"
+    try: file = open(name, "r")
+    except: return error(name)
+    lines = file.read().splitlines()
+    file.close()
+    if not lines: return error("words")
+    print(logo[1:])
     loop = asyncio.get_event_loop()
     try: loop.run_until_complete(start(token))
     except: clear()
     loop.run_until_complete(on_end())
     loop.run_until_complete(bot.logout())
 
-async def play():
-    global wait; global word; global words; global taken; global channel
-    order = ["meme", "write", "shuffle"]
-    random.shuffle(words)
-    i = 0
-    while True:
-        string = ""
-        delay = 60 * wait
-        random.shuffle(order)
-        mode = order[0]
-        if mode != "meme":
-            word = words[i].upper()
-            try: string = f"Simon says emote  {unicodedata.lookup(word)}"
-            except: pass
-            if not string and mode == "write": string = f"Simon Says write *{word}*."
-            elif not string and mode == "shuffle":
-                shuffled = list(word)
-                random.shuffle(shuffled)
-                string = f"Simon Says descramble *{''.join(shuffled)}*."
-            await printer_2(channel, string)
-            if i == len(words) - 1: i = 0
-            else: i += 1
-        else: word = "send a meme"; await printer_2(channel, f"Simon Says {word}!")
-        await asyncio.sleep(delay)
-        taken = []
-
 @bot.event
 async def on_ready():
-    global bot; global channel; global instructions
+    global channel, instructions
     name = "simon-says"
     guild = bot.guilds[0]
     for text_channel in guild.text_channels:
         if text_channel.name == name: channel = text_channel; break
     if not channel: channel = await guild.create_text_channel(name)
     await channel.purge()
-    await printer_2(channel, "Online  :white_check_mark:")
+    await printer(channel, "Online :white_check_mark:")
     await asyncio.sleep(1)
-    await printer_2(channel, instructions, True)
+    await printer(channel, instructions, True)
     await asyncio.sleep(1)
     await play()
 
 async def on_end():
-    global bot; global channel
+    global channel
     await channel.purge()
-    await printer_2(channel, "Offline  :octagonal_sign:")
+    await printer(channel, "Offline :octagonal_sign:")
 
-async def sort_scores(name, id, points = 0):
+async def play():
+    global wait, word, trick, taken, channel
+    order = ["write", "shuffle"]
+    while True:
+        string = ""
+        delay = 60 * wait
+        mode = random.choice(order)
+        if random.random() < 0.2: mode = "vote"
+        if random.random() < 0.2: mode = "meme"
+        file = open("Random.txt", "r")
+        lines = file.read().splitlines()
+        file.close()
+        word = random.choice(lines).upper()
+        if mode in order or mode == "meme":
+            string = ""
+            if mode != "meme":
+                emoji = word = word.upper()
+                emoji = emoji.encode("unicode-escape").decode("ascii")
+                if "\\" in emoji:
+                    string = f"Simon says emote {word}"
+                elif mode == "write":
+                    string = f"Simon says write *{word}*"
+                else:
+                    shuffled = list(word)
+                    random.shuffle(shuffled)
+                    shuffled = "".join(shuffled)
+                    string = f"Simon says descramble *{shuffled}*"
+            else: word = "send a meme"; string = f"Simon says {word}!"
+            if random.random() < 0.2:
+                string = string.replace("Simon says ", "").capitalize()
+                trick = True
+            await printer(channel, string)
+        else:
+            word = "vote suggestions"
+            await printer(channel, f"Simon says I need you to vote in some new words... start typing!")
+        await asyncio.sleep(delay)
+        votes = taken = []
+        trick = False
+
+async def sort_scores(id, points = 0):
+    name = "Scores.txt"
     file = open(name, "r")
-    lines = file.readlines()
+    lines = file.read().splitlines()
     file.close()
-    file = open(name, "w")
-    found = False; temp_1 = points
+    new = points
+    found = False
     for i in range(0, len(lines)):
-        line = lines[i].strip()
-        temp_2 = line
-        line = line.split("-")
-        temp_1 = str(int(line[1]) + points)
+        line = lines[i].split("-")
         if id == int(line[0]):
-            lines[i] = f"{line[0]}-{temp_1}"
-            found = True
-        else: lines[i] = temp_2
+            new = int(line[1]) + points
+            lines[i] = f"{line[0]}-{new}"
+            found = True; break
+        else: lines[i] = lines[i] = "-".join(line)
     if not found: lines.append(f"{id}-{points}")
     lines = sorted(lines, key = lambda line: int(line.split("-")[1]), reverse = True)
+    file = open(name, "w")
     file.write("\n".join(lines))
     file.close()
-    if points == 0: return lines
-    else: return temp_1
+    return new
 
-async def check_scores(ctx, arg):
+async def check_scores(ctx, arg, mode):
     count = 0
-    mode = arg
-    id_1 = ctx.message.author.id
-    string = ""; name = "Scores.txt"
-    lines = await sort_scores(name, id_1)
-    file = open(name, "r+")
-    if type(arg) == int: mode = "id"
-    elif arg != "all": mode = "name"
-    if mode in ["id", "name", "all"]:
-        for i in range(0, len(lines)):
-            line = lines[i].split("-")
-            id_2 = int(line[0])
-            name = await bot.fetch_user(id_2)
-            name = str(name.display_name)
-            if mode == "id" and id_1 == id_2:
-                string = f"You: {line[1]}"; break
-            elif mode != "id" and arg in name.lower() or mode == "all":
-                name = json.dumps(name.capitalize())[1:-1]
-                text = f"{name}: {line[1]}"
-                if count == 0: string = f"{text}"
-                if count == 1: string = f"\n• {string}\n"
-                if count >= 1: string += f"• {text}\n"
-                count += 1
-    else: string = "Error: incorrect arguments!"
-    if not string and mode == "name": string = "Error: no players found!"
-    await printer_2(ctx, string)
+    string = ""
+    await sort_scores(ctx.message.author.id)
+    file = open("Scores.txt", "r")
+    lines = file.read().splitlines()
     file.close()
+    for i in range(0, len(lines)):
+        line = lines[i].split("-")
+        id = int(line[0])
+        name = await bot.fetch_user(id)
+        name = str(name.name).lower()
+        if mode == "me" and arg == id:
+            string = f"You: {line[1]}"; break
+        elif mode == "name" and arg in name or mode == "all":
+            text = fr"{name.capitalize()}: {line[1]}"
+            if count == 0: string = f"{text}"
+            if count == 1: string = f"\n• {string}\n"
+            if count >= 1: string += f"• {text}\n"
+            count += 1
+    if not string and mode == "name": string = "Error: no players found!"
+    await printer(ctx.author, string)
+
+async def update_scores(author, won):
+    global taken
+    id = author.id
+    if id not in taken:
+        taken.append(id)
+        total = await sort_scores(id, won)
+        string = f"Congrats! You won *{won}* points. You now have *{total}* points total."
+        if won < 0: string = f"I did not say 'Simon says' so you lost *{abs(won)}* points. You now have *{total}* points total."
+        await printer(author, string)
+    else: await printer(author, f"You already got your points!")
+
+@bot.event
+async def on_reaction_add(reaction, user):
+    global word, votes, special
+    if user != bot.user:
+        if special == reaction.message:
+            if reaction.emoji == word: await update_scores(user, 10)
+        elif (word == "vote suggestions" and reaction.emoji == "\u2705"
+        and reaction.message in votes):
+            message = reaction.message
+            react = get(message.reactions, emoji = reaction.emoji)
+            if react and react.count >= 2:
+                votes.remove(message)
+                await update_scores(message.author, 20)
+                await message.add_reaction("\U0001f31f")
+                raw = message.content.lower()
+                raw = raw.encode("unicode-escape").decode("ascii")
+                file = open("Random.txt", "r+")
+                lines = file.read().splitlines()
+                if raw not in lines: file.write(f"\n{raw}")
+                file.close()
 
 @bot.command(pass_context = True)
 async def simon(ctx, *arg):
-    global bot; global channel
+    global channel, instructions
     message = ctx.message
+    author = message.author
     if message.channel == channel:
-        if arg[0] == "score" and len(arg) <= 2:
-            if len(arg) == 1 or arg[1].lower() == "me":
-                await check_scores(ctx, int(message.author.id))
-            else: await check_scores(ctx, arg[1].lower())
-        else: await printer_2(ctx, "Error: incorrect arguments!")
+        if arg[0] == "help" and len(arg) == 1:
+            await printer(author, instructions)
+        elif arg[0] == "score" and len(arg) <= 2:
+            mode = ""
+            if len(arg) == 2: mode = arg[1].lower()
+            if len(arg) == 1 or mode == "me":
+                await check_scores(ctx, message.author.id, "me")
+            elif mode == "all":
+                await check_scores(ctx, mode, "all")
+            else:
+                await check_scores(ctx, mode, "name")
+        else: await printer(author, "Error: incorrect arguments!")
 
 @bot.event
 async def on_message(message):
-    global bot; global word; global taken; global channel
-    author = message.author; id = author.id
-    if author != bot.user and message.channel == channel:
-        points_1 = 0
-        raw = message.content.upper()
-        try: raw = unicodedata.name(raw)
-        except: pass
-        if word == "send a meme":
-            try: meme = message.attachments[0].url; points_1 = 5
-            except: pass
-        elif raw == word: points_1 = 10
-        if points_1 != 0 and id not in taken:
-            taken.append(id)
-            points_2 = await sort_scores("Scores.txt", author.id, points_1)
-            await printer_2(author, f"Congrats! You won *{points_1}* points. You now have *{points_2}* points total.")
-        elif points_1 != 0 and id in taken: await printer_2(author, f"You already got your points!")
+    global word, votes, trick, special, channel
+    author = message.author
+    raw = message.content.upper()
+    if author != bot.user and message.channel == channel and "!" not in raw:
+        plain = raw.encode("unicode-escape").decode("ascii")
+        if word != "vote suggestions":
+            won = 0
+            if word == "send a meme" and message.attachments: won = 15
+            elif raw == word: won = 10
+            if won != 0 and trick: won = -5
+            if won != 0: await update_scores(author, won)
+        elif plain.isascii() and " " not in plain and plain.count("\\") <= 1:
+            votes.append(message)
+            await message.add_reaction("\u2705")
+        else:
+            await printer(author, "Invalid format!")
+            await message.delete()
+    elif author == bot.user: special = message
     await bot.process_commands(message)
 
 sys.exit(main())
